98fdc67bd5e8836e163b621caf153b2fa383dc47
aina2525 <aiaina.52@icloud.com>
2025-12-11 15:15:20 +0900

    注文数の追加

--- models/order.py
+++ models/order.py
@@ -1,4 +1,4 @@
-from peewee import Model, ForeignKeyField, DateTimeField
+from peewee import Model, ForeignKeyField, DateTimeField, IntegerField
 from .db import db
 from .user import User
 from .product import Product
@@ -6,6 +6,8 @@ from .product import Product
 class Order(Model):
     user = ForeignKeyField(User, backref='orders')
     product = ForeignKeyField(Product, backref='orders')
+    # 【追加:注文数】注文数 (数量) を保存するフィールド
+    order_quantity = IntegerField(default=1)  # 整数型のフィールドとして追加し、デフォルト値を1に設定
     order_date = DateTimeField()
 
     class Meta:
